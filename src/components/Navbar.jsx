b8b5f9d40f621299eff718dc13004d39b6ed9adc
JCapito <joshuaecapito22@gmail.com>
2024-09-06 23:01:23 +0100

    fix: Fixed the alignment of the Navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,48 +3,50 @@ import { BsCart3 } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-200">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="bg-base-200">
+      <div className="navbar align-element">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              <li>Item</li>
+              <li>Item</li>
+              <li>Item</li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
-          >
-            <li>Item</li>
-            <li>Item</li>
-            <li>Item</li>
-          </ul>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal">Nav Links</ul>
-      </div>
-      <div className="navbar-end mr-5">
-        <NavLink>
-          <div className="indicator">
-            <BsCart3 className="h-6 w-6" />
-            <span className="badge badge-sm badge-primary indicator-item">
-              3
-            </span>
-          </div>
-        </NavLink>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal">Nav Links</ul>
+        </div>
+        <div className="navbar-end mr-5">
+          <NavLink>
+            <div className="indicator">
+              <BsCart3 className="h-6 w-6" />
+              <span className="badge badge-sm badge-primary indicator-item">
+                3
+              </span>
+            </div>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
