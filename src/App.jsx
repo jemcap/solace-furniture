7850bdabd9a769065f73e227e69aa037edc310f1
JCapito <joshuaecapito22@gmail.com>
2024-09-06 15:28:54 +0100

    feature: Added createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
+import {
+  HomeLayout,
+  About,
+  Landing,
+  Cart,
+  Checkout,
+  Error,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      { path: "about", element: <About /> },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  { path: "login", element: <Login />, errorElement: <Error /> },
+  { path: "register", element: <Register />, errorElement: <Error /> },
+]);
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl">Tailwind Project</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
