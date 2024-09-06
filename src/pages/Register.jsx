9876560a3f33f79c8470a3ab47fbbd604dda1c40
JCapito <joshuaecapito22@gmail.com>
2024-09-06 21:46:27 +0100

    feature: Added a button to redirect to the Landing page when in Login or Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import { FormInput, SubmitButton } from "../components";
 import { Form, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  const handleNavigateToHome = () => {
+    navigate("/");
+  };
   return (
     <section className="h-screen grid place-items-center">
       <Form
         method="post"
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
+        <button
+          className="btn btn-sm btn-circle btn-outline"
+          onClick={handleNavigateToHome}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-chevron-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+            />
+          </svg>
+        </button>
+
         <h4 className="text-center text-3xl font-bold capitalize">Register</h4>
         <FormInput type="text" label="username" name="username" />
         <FormInput type="email" label="email" name="email" />
