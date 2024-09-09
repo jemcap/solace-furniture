304549dc65759461bbeabbb278633ed99a0fc60d
JCapito <joshuaecapito22@gmail.com>
2024-09-09 11:55:06 +0100

    feature: Implemented global loading component

--- src/components/index.js
+++ src/components/index.js
@@ -10,3 +10,4 @@ export { default as DiscoverProductHero } from "./DiscoverProductHero";
 export { default as Filters } from "./Filters";
 export { default as PaginationContainer } from "./PaginationContainer";
 export { default as ProductsContainer } from "./ProductsContainer";
+export { default as Loading } from "./Loading";
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Loading } from "../components";
 
 const HomeLayout = () => {
+  const { state } = useNavigation();
+  console.log(state);
+  const isLoading = state === "loading";
   return (
     <>
       <Header />
       <Navbar />
-      <section className="align-element py-20">
-        <Outlet />
-      </section>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <section className="align-element py-20">
+          <Outlet />
+        </section>
+      )}
+
       <Footer />
     </>
   );
