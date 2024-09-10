6adec21ef88622eea654d4f32975130e75866b20
JCapito <joshuaecapito22@gmail.com>
2024-09-10 13:47:45 +0100

    feature: Built the Orders page and implemented authorization to view only when a user is logged in + that user can view their placed orders

---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,56 @@
+import { Form, redirect } from "react-router-dom";
+import FormInput from "./FormInput";
+import SubmitButton from "./SubmitButton";
+import { fetchUrl, formatPrice } from "../utils/utils";
+import { toast } from "react-toastify";
+import { clearCart } from "../features/cart/cartSlice";
+
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    const { name, address } = data;
+    const { user } = store.getState().user;
+    const { cartItems, orderTotal, numItemsInCart } = store.getState().cart;
+    const info = {
+      name,
+      address,
+      chargeTotal: orderTotal,
+      orderTotal: formatPrice(orderTotal),
+      cartItems,
+      numItemsInCart,
+    };
+    try {
+      const response = await fetchUrl.post(
+        "/orders",
+        { data: info },
+        { headers: { Authorization: `Bearer ${user.token}` } }
+      );
+      store.dispatch(clearCart());
+      toast.success("Order placed successfully.");
+      return redirect("/orders");
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "there was an error placing your order";
+      toast.error(errorMessage);
+      if (error?.response?.status === 401 || 403) return redirect("/login");
+      return null;
+    }
+  };
+
+const CheckoutForm = () => {
+  return (
+    <Form method="post" className="flex flex-col gap-y-4">
+      <h4 className="font-medium text-xl">Shipping Information</h4>
+      <FormInput label="first name" name="name" type="text" />
+      <FormInput label="address" name="address" type="text" />
+      <div className="mt-4">
+        <SubmitButton text="Place Your Order" />
+      </div>
+    </Form>
+  );
+};
+
+export default CheckoutForm;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { redirect, useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
+import { fetchUrl } from "../utils/utils";
+import { OrdersList, PaginationContainer, SectionTitle } from "../components";
+
+export const loader =
+  (store) =>
+  async ({ request }) => {
+    const { user } = store.getState().user;
+
+    if (!user) {
+      toast.warn("User must be logged in to view orders.");
+      return redirect("/login");
+    }
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    try {
+      const response = await fetchUrl("/orders", {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      console.log(response.data.data);
+      return { orders: response.data.data, meta: response.data.meta };
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "there was an error accessing your orders";
+      toast.error(errorMessage);
+      if (error?.response?.status === 401 || 403) return redirect("/login");
+      return null;
+    }
+  };
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { meta } = useLoaderData();
+  if (meta.pagination.total < 1) {
+    return <SectionTitle text="Please make an order" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Your orders" />
+      <OrdersList />
+      <PaginationContainer />
+    </>
+  );
 };
 
 export default Orders;
